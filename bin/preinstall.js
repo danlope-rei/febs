cf7fabb45b220f2ef8799b55e59b8068e498525c
Alex Perkins <aperkin@rei.com>
2019-08-21 10:15:11 -0700

    Update messaging.

--- bin/preinstall.js
+++ bin/preinstall.js
@@ -10,7 +10,7 @@ const enginesNodeVersion = JSON.parse(febsPackageJson.toString()).engines.node;
 const minNodeVersion = enginesNodeVersion.split('').filter(c => Number.parseInt(c, 10) >= 0).join('.');
 
 if (!lib.checkVersion(process.version, minNodeVersion)) {
-  console.error(`⛔ febs requires at least node.js ${enginesNodeVersion}. You are on version ${process.version}`);
+  console.error(`⛔ febs requires node.js ${enginesNodeVersion}. You are on version ${process.version}`);
   process.exit(1);
 }
 
