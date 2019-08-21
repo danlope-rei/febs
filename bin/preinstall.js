3e7f17ee14fca8ab731e79856bf31a6f05655efb
Alex Perkins <aperkin@rei.com>
2019-08-21 08:35:39 -0700

    Add preinstall script for version check.

--- bin/preinstall.js
+++ bin/preinstall.js
@@ -1,17 +1,18 @@
-const fsExtra = require('fs-extra');
+#!/usr/bin/env node
+
+const fs = require('fs');
 const path = require('path');
-const logger = require('../lib/logger');
 const lib = require('../lib');
 
 // Version check for node.js. Should be >= engines.node.
-const febsPackageJson = fsExtra.readJsonSync(path.join(__dirname, '..', 'package.json'));
-const enginesNodeVersion = febsPackageJson.engines.node;
+const febsPackageJson = fs.readFileSync(path.join(__dirname, '..', 'package.json'));
+const enginesNodeVersion = JSON.parse(febsPackageJson.toString()).engines.node;
 const minNodeVersion = enginesNodeVersion.split('').filter(c => Number.parseInt(c, 10) >= 0).join('.');
 
 if (!lib.checkVersion(process.version, minNodeVersion)) {
-  logger.error(`⛔ febs requires at least node.js ${enginesNodeVersion}. You are on version ${process.version}`);
+  console.error(`⛔ febs requires at least node.js ${enginesNodeVersion}. You are on version ${process.version}`);
   process.exit(1);
 }
 
 // Log the current node version.
-logger.info(`✅ Current node version: ${process.version}.`);
+console.log(`✅ Current node version: ${process.version}.`);
